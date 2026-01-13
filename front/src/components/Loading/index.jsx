bc993a149738196e47885b16f3cff6176bb841f5
Suprema UX <70323230+supremaux@users.noreply.github.com>
2026-01-13 15:38:13 -0400

    Nova loading screen

--- front/src/components/Loading/index.jsx
+++ front/src/components/Loading/index.jsx
@@ -2,15 +2,25 @@
 import React from "react";
 import styles from "./LoadingScreen.module.css";
 import { Spinner } from "react-bootstrap";
+import Logo from "../../assets/logo.png";
 
 export const LoadingScreen = () => {
   return (
     <div className={styles.loadingOverlay}>
       <div className={styles.loadingContent}>
+        <picture>
+          <source media="(max-width: 767px)" srcSet={Logo} />
+          <img
+            src={Logo}
+            alt="Logo"
+            className={styles.logo + "img-fluid mb-4"}
+            loading="lazy"
+            decoding="async"
+          />
+        </picture>
         <Spinner animation="border" role="status" variant="primary">
           <span className="visually-hidden">Carregando...</span>
         </Spinner>
-        <p>Carregando o aplicativo...</p>
       </div>
     </div>
   );
